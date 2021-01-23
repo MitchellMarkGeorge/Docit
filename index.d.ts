f306157abfe2afc12f81dc1f91cbd3ae2ca56dad
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2021-01-23 11:27:35 -0500

    added extra method for library

--- index.d.ts
+++ index.d.ts
@@ -2,13 +2,15 @@ declare module docit {
   function move(): void;
   function rollback(version: string): void;
   function peek(version: string): void;
-  function init(documentPath: string, alias: string): void;
+  function init(documentPath: string, alias?: string): void;
   function open(alias: string): void;
-  function new_version(comments: string): void;
+  function new_version(comments?: string): void;
   function list_versions(): ProjectVersions;
   function close(): void;
   function status(): ProjectConfig; // should retun something {}
   function list_projects(): string[];
+  function isInProject(): boolean
+  function make_file(path: string): void
 }
 
 interface ProjectConfig {
